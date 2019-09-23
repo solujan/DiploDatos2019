cb8ae3ca6bdfac451a8c586c8d6227910cdc9c8c
slujan <sofia.lujan@gpcloud.com>
2019-09-23 20:56:39 -0300

    erase file

--- ProyectoPobrezaCostaRica/clean.py
+++ ProyectoPobrezaCostaRica/clean.py
@@ -13,6 +13,11 @@ rename_col_dict = {
     'lugar6': 'region_huetar_norte'}
 
 
+def add_new_column(_ds, feature, new_feature):
+    print(feature)
+    _ds = _ds.assign(new_feature=_ds.groupby('idhogar')[feature].transform(lambda x: (x.mean())**2))
+    return _ds
+
 def get_filter_by_row(input, columns):
     filter_data = {}
     for col in columns:
